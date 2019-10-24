a2af9472a8cc54f410ccbb26762b3631627370d8
yuhao <ityuhao@gmail.com>
2019-10-24 16:46:31 +0800

    init

---
+++ src/main/java/com/n2o2l/sca/demo/ScaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.n2o2l.sca.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScaDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/n2o2l/sca/demo/ScaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.n2o2l.sca.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScaDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
